707bd8545d632d9e2a008cf76b4fff3022167bdb
J H <jahalu88@hotmail.com>
2018-04-07 14:51:01 -0400

    added readme and finished comments. Previous gifs will now clear out

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Giftastic
+# Disney Villains
+This project was created by Jaime Halupka as a requirement for homework during University of Central Florida's Coding
+Bootcamp. 
+
+![In Action](/assets/images/screenshot.png "In Action")
+
+**Instructions**
+Press any villain button to see six gifs related to that character. You can also type in a new name if you wish to create your own button. Each gif can then be pressed to alternate between pause and play. Each time a new villain button is pressed, the previous gifs clear. Have fun!
+
+**Acknowledgements**
+The castle photograph for this project was taken in Walt Disney World's Magic Kingdom by Jaime Halupka in the winter of 2016. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,16 @@
 
 // Initial array of Disney characters
-var movies = ["Cinderella", "Ariel", "Ursula", "Rapunzel", "Moana", "Belle", "Mickey Mouse", "Goofy" ];
+var movies = ["Maleficent", "The Evil Queen", "Ursula", "Yzma", "Jafar", "Hades", "Cruella De Vil"];
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayDisneyCharacter() {
+
+// Clears out any old gifs
+    $("#disgifdiv").empty();
+    $("#disgifdiv2").empty();
     var disneychoice = $(this).attr("data-name");
-    // var movie = $(this).attr("data-name");
+
+// Giphy API
 var getDis = "https://api.giphy.com/v1/gifs/search?api_key=xo2pGkkJnrWklqW1H9jNsX02IcWREuIV&q=" + disneychoice + "&limit=6&offset=0&rating=G&lang=en";
 
 $.ajax({
@@ -15,6 +20,8 @@ $.ajax({
         console.log(response);
         
         var disresults = response.data
+
+//  Code for displaying gifs left of image  
         for (i=0; i<6; i++) {
             if (i<3) {
             var gifDiv = $("<div class='item'>");
@@ -23,32 +30,36 @@ $.ajax({
             disimage.attr("data-still", disresults[i].images.original_still.url);
             disimage.attr("data-animate", disresults[i].images.original.url);
             disimage.attr("data-state", "still");
+            var p = $("<p class=rating>").text("Rating: " + disresults[i].rating);
             gifDiv.append(disimage);
-            $("#disgifdiv").prepend(gifDiv);}
-        else
-     {       
-        var gifDiv2 = $("<div class='item'>");
-        var disimage = $("<img class=gif>");
-        disimage.attr("src", disresults[i].images.original_still.url);           
-        disimage.attr("data-still", disresults[i].images.original_still.url);
-        disimage.attr("data-animate", disresults[i].images.original.url);
-        disimage.attr("data-state", "still");
-        gifDiv2.append(disimage);
-        $("#disgifdiv2").prepend(gifDiv2);
-        }};
+            $("#disgifdiv").prepend(gifDiv);
+            gifDiv.append(p);}
 
+// Code for displaying gifs right of image            
+        else {       
+            var gifDiv2 = $("<div class='item'>");
+            var disimage = $("<img class=gif>");
+            disimage.attr("src", disresults[i].images.original_still.url);           
+            disimage.attr("data-still", disresults[i].images.original_still.url);
+            disimage.attr("data-animate", disresults[i].images.original.url);
+            disimage.attr("data-state", "still");
+            var p = $("<p class=rating>").text("Rating: " + disresults[i].rating);
+            gifDiv2.append(disimage);
+            $("#disgifdiv2").prepend(gifDiv2);
+            gifDiv2.append(p);
+            }};
 
+// Pausing and startign gifs when clicked
         $(".gif").on("click", function() {
-            var state = $(this).attr("data-state");
-      if (state == "still") {
-     $(this).attr("src", $(this).attr("data-animate"));
-     $(this).attr("data-state", "animate");
-      }
-     else
-     {
-       $(this).attr("src", $(this).attr("data-still"));
-       $(this).attr("data-state", "still");
-        }});
+        var state = $(this).attr("data-state");
+        if (state == "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+             }
+         else
+            {$(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            }});
        
     });
 };
@@ -68,25 +79,21 @@ $.ajax({
             a.attr("data-name", movies[i]);
             a.text(movies[i]);
             $("#buttons-view").append(a);
-           
         }
     }
 
 
-        
+ // Adding a new button from user's input
 
     $("#add-disney").on("click", function(event) {
         event.preventDefault();
-        
         var disneychoice = $("#disney-input").val().trim();
-       
         movies.push(disneychoice);
-        
         $("#input-form")[0].reset();
         renderbuttons();
-       
       });
 
+//Calling initial rendering of array buttons and movie button clicks
       $(document).on("click", ".movie-btn", displayDisneyCharacter);
       renderbuttons();
 
